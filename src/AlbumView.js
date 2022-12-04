1b69ff73d3a96ec0d8b7d3c8e887c85d53dcb4c1
Victoria Beasley <iesnoth91@gmail.com>
2022-12-03 18:11:59 -0600

    finished rendering artist view

--- src/AlbumView.js
+++ src/AlbumView.js
@@ -5,6 +5,16 @@ function AlbumView() {
     const { id } = useParams()
     const [albumView, setAlbumView] = useState([])
 
+    useEffect(() => {
+        const API_URL = `http://localhost:4000/song/${id}`
+        const fetchData = async () => {
+            const response = await fetch(API_URL)
+            const resData = await response.json()
+            console.log(resData)
+        }
+        fetchData()
+    },[id])
+
     return(
         <div>
             <h2>The album id passed was: {id}</h2>
